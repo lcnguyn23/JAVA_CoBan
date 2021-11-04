e0cd23b2e8f837eb2f124319111bdb051b5a8072
lcnguyn23 <locnxt01@gmail.com>
2021-11-04 14:55:23 +0700

    Bai 1

---
+++ OOPSwing/src/bai1/HelloWorldSwing.java
@@ -0,0 +1,52 @@
+package bai1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class HelloWorldSwing extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					HelloWorldSwing frame = new HelloWorldSwing();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HelloWorldSwing() {
+		setTitle("Hello world");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JLabel lblNewLabel = new JLabel("Hello World");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 36));
+		contentPane.add(lblNewLabel, BorderLayout.CENTER);
+	}
+
+}
